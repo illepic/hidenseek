4986e225f9858e882d2b07dfae93f253ca94d76a
Christopher Bloom <bloomcb@gmail.com>
2018-02-05 19:22:38 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var qr = require('qr-image');
+ 
+var qr_png = qr.image('I love QR!', { type: 'png' });
+
+qr_png.pipe(require('fs').createWriteStream('i_love_qr.png'));
+ 
+// var svg_string = qr.imageSync('I love QR!', { type: 'png' });
